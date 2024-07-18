e2f75acb6383c1f454b625ed41247db01b4ab8d9
mehmet <mehmet00arslan@gmail.com>
2024-07-18 20:38:02 +0300

    object

--- object.js
+++ object.js
@@ -1,1 +1,23 @@
 console.log("***********OBJECT*************");
+const ogrisim = ["ahmet", "mehmet", "saffet"]
+const ogrsoyisim = ["yılmaz", "can", "baki"]
+
+console.log(`${ogrisim[1]} - ${ogrsoyisim[1]}`);
+
+
+/* ---------------------------------- */
+/*     Object Oluşturma Yöntemleri    */
+/* ---------------------------------- */
+
+// 3 farkli yöntem ile object oluşturulabilir
+
+// 1. object() class'ından türetme
+
+const car = new Object(); // boş bir obje üretir
+
+
+car.brand ="BMW"
+car.model = 1990
+car.price = 10000
+console.log(car);
+console.log(car.model);
